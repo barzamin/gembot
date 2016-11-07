2e8a5dabcc5ac848313a72c7c32b8862da291553
Erin <sylphofelectricity@gmail.com>
2016-11-06 23:44:12 -0600

    Add Config.@client_id, get it connecting to Discord

--- src/gembot/bot.cr
+++ src/gembot/bot.cr
@@ -1,4 +1,16 @@
+require "discordcr"
+
 module Gembot
   class Bot
+    def initialize(config : Config)
+      @config = config
+      @client = Discord::Client.new(token: @config.token, client_id: @config.client_id)
+      @client.cache = Discord::Cache.new(@client)
+    end
+
+    # Kick off the main Discord event loop
+    def run!
+      @client.run
+    end
   end
 end
