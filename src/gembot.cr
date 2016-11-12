62ae38dad713ff5db3590a24750a78f09bb2ba97
Erin <sylphofelectricity@gmail.com>
2016-11-11 22:37:59 -0600

    Remove startup code from package root

--- src/gembot.cr
+++ src/gembot.cr
@@ -1,10 +1,1 @@
-require "dotenv"
 require "./gembot/*"
-
-Dotenv.load
-config = Gembot::Config.from_yaml(File.read("./config.yml"))
-config.token = ENV["TOKEN"]
-config.client_id = ENV["CLIENT_ID"].to_u64
-
-bot = Gembot::Bot.new(config)
-bot.run!
