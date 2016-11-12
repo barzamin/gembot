abc8e0738ff987579cdc8cc62e2d0b9ed932d8fb
Erin <sylphofelectricity@gmail.com>
2016-11-11 22:39:42 -0600

    Add plugin-loading to Gembot::Bot

--- src/gembot/bot.cr
+++ src/gembot/bot.cr
@@ -2,11 +2,20 @@ require "discordcr"
 
 module Gembot
   class Bot
+    property plugins
+
     def initialize(config : Config)
       @config = config
+
       @client = Discord::Client.new(token: @config.token, client_id: @config.client_id)
       @cache = Discord::Cache.new(@client)
       @client.cache = @cache
+
+      @plugins = [] of Gembot::Plugin
+    end
+
+    def load_plugins!
+      @plugins.each { |p| p.hook(@client) }
     end
 
     # Kick off the main Discord event loop
