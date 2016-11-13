5f264be8fb6559d204d29c3db7e6c12bdd82afd6
Erin <sylphofelectricity@gmail.com>
2016-11-12 18:13:30 -0600

    Merge handler_data.cr into handler.cr; implement Handlers, triggers, RClient

--- src/gembot/handler.cr
+++ src/gembot/handler.cr
@@ -1,14 +1,39 @@
 module Gembot
+  alias EventData = Discord::Message
+
   class Handler
     alias HandlerCallback = Proc(RClient, EventData, Nil)
+    getter etype
+    getter callback
 
-    def initialize(@etype : Symbol, @condition : Regex?, @block : HandlerCallback)
+    def initialize(@etype : Symbol, @condition : Regex?, @callback : HandlerCallback)
     end
 
-    def initialize(@etype : Symbol, @block : HandlerCallback)
+    def initialize(@etype : Symbol, @callback : HandlerCallback)
     end
 
-    def hook(client)
+    def triggered?(edat : EventData)
+      case edat
+      when Discord::Message
+        return !(edat.content =~ @condition).nil?
+      end
+    end
+  end
+
+  class RClient
+    def initialize(@client : Discord::Client)
+    end
+
+    def reply(to : Discord::Message, text)
+      @client.create_message(to.channel_id, text)
+    end
+
+    def reply!(to : Discord::Message, text)
+      @client.create_message(to.channel_id, mention(to.author) + ": " + text)
+    end
+
+    def mention(user : Discord::User)
+      "<@#{user.id}>"
     end
   end
 end
--- src/gembot/handler_data.cr
+++
@@ -1,8 +0,0 @@
-module Gembot
-  alias EventData = Discord::Message
-
-  class RClient
-    def reply(to : Discord::Message, text)
-    end
-  end
-end
