73eb6a3e701ca99af12ff78590ba68445a38a58a
Erin <sylphofelectricity@gmail.com>
2016-11-13 20:19:15 -0600

    [Gembot::RClient] Implement many guild/role related helpers

--- src/gembot/handler.cr
+++ src/gembot/handler.cr
@@ -35,6 +35,53 @@ module Gembot
       @client.create_message(to.channel_id, mention(to.author) + ": " + text)
     end
 
+    def create_role(guild : Discord::Guild, name : String?)
+      role = @client.create_guild_role(guild.id)
+      unless name.nil?
+        role.name = name
+        update_role(guild, role)
+      end
+      role
+    end
+
+    def get_role(guild : Discord::Guild, name : String)
+      role = get_roles(guild).find {|r| r.name == name}
+    end
+
+    def get_roles(guild : Discord::Guild)
+      @cache.guild_roles(guild.id).map {|r| @cache.resolve_role(r)}
+    end
+
+    def update_role(guild : Discord::Guild, role : Discord::Role)
+      @client.modify_guild_role(guild.id, role.id, role.name, role.permissions, role.colour, role.position, role.hoist)
+    end
+
+    def fetch_or_create_role(guild : Discord::Guild, name : String)
+      role = get_role(guild, name: name)
+      if role.nil?
+        role = create_role(guild, name)
+      end
+      role
+    end
+
+    def role_add_user(guild : Discord::Guild, role : Discord::Role, user : Discord::User)
+      member = get_member(guild, user)
+      @client.modify_guild_member(guild.id, user.id,
+            nick: nil, roles: member.roles | [role.id], mute: nil, deaf: nil, channel_id: nil)
+    end
+
+    def guild_id_of(m : Discord::Message)
+      @cache.resolve_channel(m.channel_id).guild_id
+    end
+
+    def guild_of(m : Discord::Message)
+      @cache.resolve_guild(guild_id_of(m).not_nil!)
+    end
+
+    def get_member(guild : Discord::Guild, user : Discord::User)
+      @cache.resolve_member(guild.id, user.id)
+    end
+
     def mention(user : Discord::User)
       "<@#{user.id}>"
     end
